a014e2c1af82d10b8717979a7e33ead4e1980fe9
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-03-30 19:55:16 +0200

    passing events with params and noparams, invoked events need to be in a fat arrow func

--- src/index.js
+++ src/index.js
@@ -40,11 +40,21 @@ function App() {
 
 function Book(props) {
   const { title, author, image } = props;
+
+  const clickHandler = () => {
+    console.log(title);
+  };
+
+  const mouseHandler = author => {
+    console.log(author);
+  };
   return (
     <div>
       <p style={{ textAlign: 'center', width: 300 }}>{title}</p>
-      <p style={{ textAlign: 'center', width: 300 }}>{author}</p>
-      <img width={300} src={image}></img>
+      <p style={{ textAlign: 'center', width: 300 }} onMouseOver={()=>mouseHandler(author)}>
+        {author}
+      </p>
+      <img width={300} src={image} onClick={clickHandler}></img>
     </div>
   );
 }
